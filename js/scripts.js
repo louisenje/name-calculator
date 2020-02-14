e4c312187ef3e8590ace08b115cd39c83c8e6a14
louisenje <louisenje@gmail.com>
2020-02-14 12:44:00 +0300

    naming arrays:daysoftheweek,femalenames and malenames

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var daysOfWeek=[
+    "sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+];
+var maleNames=[
+     "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+];
+var femaleNames=[
+     "Akosua", "Adwoa", "Abenaa", "Akua","Yaa","Afua","Ama"
+];
+//calling functions
+function getName(){
+    var year= parseInt(document.getElementById("year").value)
+    var month= parseInt(document.getElementById("month").value)
+    var day= parseInt(document.getElementById("day").value)
+    var male=document.getElementById("male")
+    var female=document.getElementById("male")
+//validation
+if (day<=0||day>31)
+alert("pleas enter the correct day");
+else if(month<0||month>12||month==2 && day>29)
+alert ("[Please enter the correct month");
+var day= new Date(year + "/" + month+ "/" + day);
+var birthDay=day.getDay();
+if (male.checked==true){
+    alert( "You were born on" + daysOfWeek[birthDay] + "and your akan name is"+ maleNames[birthDay])
+}
+else if(female.checked==true){
+    alert( "You were born on" + daysOfWeek[birthDay] + "and your akan name is + femaleNames[birthDay])
+}
+}
