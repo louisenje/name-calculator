58d9148cef7f23ec8aec0cae84defda6c4e5ff24
louisenje <louisenje@gmail.com>
2020-02-14 15:27:56 +0300

    form for date,year and month

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var daysOfWeek=[
-    "sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+    "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
 ];
 var maleNames=[
      "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
@@ -10,21 +10,27 @@ var femaleNames=[
 //calling functions
 function getName(){
     var year= parseInt(document.getElementById("year").value)
+    
     var month= parseInt(document.getElementById("month").value)
+    
     var day= parseInt(document.getElementById("day").value)
-    var male=document.getElementById("male")
-    var female=document.getElementById("male")
+    
+    var male= document.getElementById("male")
+    
+    var female= document.getElementById("female")
+    
+
 //validation
 if (day<=0||day>31)
-alert(pleas enter the correct day");
+alert("please enter the correct date");
 else if(month<0||month>12||month==2 && day>29)
-alert ("[Please enter the correct month");
+alert ("Please enter the correct month");
 var day= new Date(year + "/" + month+ "/" + day);
 var birthDay=day.getDay();
 if (male.checked==true){
-    alert( "You were born on" + daysOfWeek[birthDay] + "and your akan name is"+ maleNames[birthDay])
+    alert( "You were born on" +daysOfWeek[birthDay]+ "and your akan name is"+ maleNames[birthDay])
 }
-else if(female.checked==true)
-    alert( "You were born on" + daysOfWeek[birthDay] + "and your akan name is + femaleNames[birthDay])
+else if(female.checked==true){
+    alert( "You were born on"+ daysOfWeek[birthDay] +"and your akan name is" + femaleNames[birthDay])
 }
 }
